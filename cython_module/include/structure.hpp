424c11600ced82760ca7e6d345beb0a87f2c51e7
czq01 <czq0106@outlook.com>
2023-09-24 19:37:49 -0700

    Feat: Union Structure
    1. Union Structure Finshed, compile-time optimized
    2.  RSI_Calculator Finished.

--- cython_module/include/structure.hpp
+++ cython_module/include/structure.hpp
@@ -10,6 +10,7 @@
 #include <numeric>
 #include <functional>
 #include "util.hpp"
+#include <initializer_list>
 #define CACHE_LINE_SIZE 64
 #define _CACHE_ALIGN alignas(CACHE_LINE_SIZE)
 
@@ -18,224 +19,6 @@
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 #endif
-
-// ---------------------------------------
-// **************************************
-// ********** Single Structure ***********
-// **************************************
-
-class SingleArrayManager_t {
-    int pointer;
-    double * closes;
-    double sum;
-    double sum_square;
-public:
-    bool array_inited;
-    int max_size;
-    int size;
-    double high;
-    double low;
-
-    SingleArrayManager_t(int size=0):
-        max_size(size), closes(new double[size]), pointer(-1), array_inited(false),
-        sum(0), sum_square(0), size(0), high(-INFINITY), low(INFINITY) {}
-
-    SingleArrayManager_t(const SingleArrayManager_t&) = delete;
-
-    SingleArrayManager_t(SingleArrayManager_t&& am):
-        max_size(am.max_size), pointer(am.pointer), array_inited(am.array_inited),
-        sum(am.sum), sum_square(am.sum_square), high(am.high), low(am.low),
-        size(am.size) {
-        double * _tmp_close = closes;
-        closes = am.closes;
-        am.closes = _tmp_close;
-    }
-
-    SingleArrayManager_t& operator=(const SingleArrayManager_t &) = delete;
-
-    SingleArrayManager_t& operator=(SingleArrayManager_t && am) {
-        max_size = am.max_size;
-        pointer = am.pointer;
-        array_inited = am.array_inited;
-        sum = am.sum;
-        sum_square = am.sum_square;
-        double * tmp = closes;
-        closes = am.closes;
-        am.closes = tmp;
-        return *this;
-    }
-
-    constexpr double get_close() {return this->closes[this->pointer];}
-
-    constexpr double get_close(const int offset) {
-        if (pointer>=offset) return closes[pointer-offset];
-        else return closes[pointer-offset+max_size];
-    }
-
-    constexpr void update_bar(const BarData& bar) {
-        size++; pointer++;
-        if (pointer == max_size) [[unlikely]] {pointer=0;array_inited=true;}
-        if (array_inited) [[likely]] {
-            sum-=closes[pointer];
-            sum_square -= closes[pointer]*closes[pointer];
-            size--;
-        }
-        closes[pointer] = bar.close;
-        sum += bar.close;
-        sum_square += bar.close*bar.close;
-        high = std::max(bar.high, high);
-        low = std::min(bar.low, low);
-    };
-
-    constexpr double get_mean() {return sum/size;}
-
-    constexpr double get_mean(int N, int offset=0) {
-        double _sum = 0;
-        int _p = this->pointer;
-        int n = std::min(N, size-offset);
-        N = n;
-        while (offset) {_p--; offset--;}
-        while (N) {
-            if (_p<0) {_p+= this->max_size;}
-            _sum += this->closes[_p];
-            N--; _p--;
-        }
-        return _sum/n;
-    }
-
-    constexpr double get_std() {
-        return sqrt_const((sum_square - get_mean()*sum)/(size-1));
-    }
-
-    constexpr double get_std(int N, int offset=0) {
-        double _sum_sq = 0;
-        int _p = this->pointer;
-        int n = std::min(N, size-offset);
-        N = n;
-        while (offset) {_p--;offset--;}
-        while (N) {
-            if (_p<0) {_p+= this->max_size;}
-            _sum_sq += this->closes[_p]*this->closes[_p];
-            N--; _p--;
-        }
-        int mean = get_mean(n, offset);
-        return sqrt_const((_sum_sq - mean*mean*n)/(n-1));
-    }
-
-    ~SingleArrayManager_t() {
-        delete [] closes;
-    }
-};
-
-class SingleCalculator_MACD_t: public virtual SingleArrayManager_t {
-private:
-    struct _MACDType {double MACD,DIF,DEA;};
-    int _fast;
-    int _slow;
-    int _count;
-    double _EMA_fast;
-    double _EMA_slow;
-    double _MACD;
-    double _DEA;
-    double _DIF;
-public:
-    bool inited;
-    typedef _MACDType _IndexReturnType;
-
-    SingleCalculator_MACD_t(int fast, int slow):
-        _fast(fast), _slow(slow), _count(1), _DEA(0), inited(false),
-        _EMA_fast(NAN), _EMA_slow(NAN), _MACD(NAN), _DIF(NAN) {}
-
-    void update() {
-        if (this->inited) [[likely]] {
-            _EMA_fast = (_EMA_fast*(_fast-1)+2*this->get_close())/(_fast+1);
-            _EMA_slow = (_EMA_slow*(_slow-1)+2*this->get_close())/(_slow+1);
-            _DIF = _EMA_fast-_EMA_slow;
-            _MACD = (_MACD*8 + _DIF*2)/10;
-            _DEA = _DIF - _MACD;
-            return;
-        }
-        if (std::isnan(_EMA_fast)) [[unlikely]] {
-            _EMA_fast = this->get_mean(_fast);
-            _EMA_slow = this->get_mean(_slow);
-            _DIF = _EMA_fast - _EMA_slow;
-        } else [[likely]] {
-            _EMA_fast = (_EMA_fast*(_fast-1)+2*this->get_close())/(_fast+1);
-            _EMA_slow = (_EMA_slow*(_slow-1)+2*this->get_close())/(_slow+1);
-            _count++;
-            if (_count < 9) {
-                _DIF += _EMA_fast-_EMA_slow;
-            } else {
-                _DIF += _EMA_fast-_EMA_slow;
-                _MACD = _DIF/9;
-                _DIF = _EMA_fast-_EMA_slow;
-                _DEA = _DIF-_MACD;
-                inited = true;
-            }
-        }
-    }
-
-    constexpr _MACDType get_MACD() {return {_MACD, _DIF, _DEA};}
-};
-
-class SingleCalculator_RSI: public virtual SingleArrayManager_t {
-    int _period;
-public:
-    SingleCalculator_RSI(int period):
-        _period(period) {}
-};
-
-
-// ---------------------------------------
-// **************************************
-// ********** Union Structure ***********
-// **************************************
-
-template <class ..._IndexType> requires(std::is_base_of_v<SingleArrayManager_t, _IndexType> && ...)
-class SingleIndexManager;
-
-template <>
-class SingleIndexManager<>: public SingleArrayManager_t {
-    SingleIndexManager(int size):
-        SingleArrayManager_t(size) {}
-};
-
-// typedef SingleIndexManager<> ArrayMgr;
-
-template<class ..._IndexType> requires(std::is_base_of_v<SingleArrayManager_t, _IndexType> && ...)
-class SingleIndexManager: public _IndexType... {
-
-    template <class Arg>
-    constexpr bool _is_inited() { return Arg::inited; }
-
-    template <class Arg1, class ...Args> requires (sizeof...(Args)!=0)
-    constexpr bool _is_inited() { return Args::inited && _is_inited<Args...>; }
-
-    template <class Arg1, class ...Args>
-    void _update_bar() {
-        Arg1::update();
-        if constexpr(sizeof...(Args)) _update_bar<Args...>();
-    }
-
-public:
-    template <class ...Args>
-    SingleIndexManager(int max_length, Args ...args):
-        SingleArrayManager_t(max_length), _IndexType(args...)... {}
-
-    constexpr void update_bar(const BarData& bar) {
-        SingleArrayManager_t::update_bar(bar);
-        this->_update_bar<_IndexType...>();
-    }
-
-    constexpr bool is_inited() {
-        return SingleArrayManager_t::array_inited && _is_inited<_IndexType...>();
-    }
-
-};
-
-template <class ...Args>
-SingleIndexManager(Args...) -> SingleIndexManager<>;
-
 
 
 
@@ -551,13 +334,13 @@ private:
 #if CUDA_ENABLE
     // TODO
 #else
-    std::unordered_map<std::string_view, SingleArrayManager> ams;
+    std::unordered_map<std::string, SingleArrayManager> ams;
 #endif
     void * _update_func;
 
-    void onNewSymbol(const std::string_view& symbol) {
+    void onNewSymbol(const std::string& symbol) {
         int number = (*static_cast<int*>(_update_func));
-        auto funcs = (std::function<void(const std::string_view&)>**)(static_cast<int*>(_update_func)+1);
+        auto funcs = (std::function<void(const std::string&)>**)(static_cast<int*>(_update_func)+1);
         while (number) {
             (**funcs)(symbol);
             funcs++;
@@ -565,7 +348,7 @@ private:
         }
     }
 
-    void newIndexFunc(std::function<void(const std::string_view&)>* func) {
+    void newIndexFunc(std::function<void(const std::string&)>* func) {
         int val = *(int *)(this->_update_func);
         void * new_sub_arr;
         if (!(val&0b1)) {
@@ -577,7 +360,7 @@ private:
         val++;
         // set it to func
         int length = ArrMgr_SubSize(val-1);
-        *reinterpret_cast<std::function<void(const std::string_view&)>**>((char *) new_sub_arr + length) = func;
+        *reinterpret_cast<std::function<void(const std::string&)>**>((char *) new_sub_arr + length) = func;
         *(int *)new_sub_arr = val;
         delete [] this->_update_func;
         this->_update_func = new_sub_arr;
@@ -617,17 +400,17 @@ public:
     }
 
     template <typename ...Args>
-    constexpr const double get_mean(const std::string_view& symbol, Args ...args) {
+    constexpr const double get_mean(const std::string& symbol, Args ...args) {
         return ams.at(symbol).get_mean(std::forward<Args>(args)...);
     }
 
     template <typename ...Args>
-    constexpr double get_std(const std::string_view& symbol, Args ...args) {
+    constexpr double get_std(const std::string& symbol, Args ...args) {
         return ams.at(symbol).get_std(std::forward<Args>(args)...);
     }
 
     template <typename ...Args>
-    constexpr double get_close(const std::string_view& symbol, Args ...args) {
+    constexpr double get_close(const std::string& symbol, Args ...args) {
         return ams.at(symbol).get_close(std::forward<Args>(args)...);
     }
 
@@ -635,7 +418,7 @@ public:
         delete [] _update_func;
     }
 
-    bool is_inited(const std::string_view& domain) const {
+    bool is_inited(const std::string& domain) const {
         return this->ams.at(domain).is_inited;
     }
 
@@ -744,33 +527,303 @@ public:
     static_assert(std::is_base_of_v<_Base_Index_Calculator<_IndexClassType>, _IndexClassType>,
                     "Must be Inherited from Base_Index_Calculator.");
 
-    std::unordered_map<std::string_view, _IndexClassType> _syb_mapper;
+    std::unordered_map<std::string, _IndexClassType> _syb_mapper;
 
     ArrayManager * _amp;
 
-    constexpr void onNewSymbol(const std::string_view& symbol) {
+    constexpr void onNewSymbol(const std::string& symbol) {
         _syb_mapper.emplace(symbol, (_amp->ams.at(symbol)));
     }
 
 public:
 
     _Index_Calculator(ArrayManager& am): _amp(&am)  {
-        auto func = new std::function<void(const std::string_view&)>(
+        auto func = new std::function<void(const std::string&)>(
             std::bind(&_Index_Calculator::onNewSymbol, this, std::placeholders::_1));
         _amp->newIndexFunc(func);
     }
 
-    constexpr bool is_inited(const std::string_view& symbol) {
+    constexpr bool is_inited(const std::string& symbol) {
         return _syb_mapper.at(symbol).is_inited;
     }
 
-    constexpr _IndexClassType::_IndexReturnType get_val(const std::string_view& symbol) {
+    constexpr _IndexClassType::_IndexReturnType get_val(const std::string& symbol) {
         return _syb_mapper.at(symbol).get_val();
+    }
+};
+
+// ---------------------------------------
+// **************************************
+// ********** Single Structure ***********
+// **************************************
+
+class SingleArrayManager_t {
+    int pointer;
+    double * closes;
+    double sum;
+    double sum_square;
+public:
+    bool array_inited;
+    int max_size;
+    int size;
+    double high;
+    double low;
+
+    SingleArrayManager_t(int size=0):
+        max_size(size), closes(new double[size]), pointer(-1), array_inited(false),
+        sum(0), sum_square(0), size(0), high(-INFINITY), low(INFINITY) {}
+
+    SingleArrayManager_t(const SingleArrayManager_t&) = delete;
+
+    SingleArrayManager_t(SingleArrayManager_t&& am):
+        max_size(am.max_size), pointer(am.pointer), array_inited(am.array_inited),
+        sum(am.sum), sum_square(am.sum_square), high(am.high), low(am.low),
+        size(am.size) {
+        double * _tmp_close = closes;
+        closes = am.closes;
+        am.closes = _tmp_close;
+    }
+
+    SingleArrayManager_t& operator=(const SingleArrayManager_t &) = delete;
+
+    SingleArrayManager_t& operator=(SingleArrayManager_t && am) {
+        max_size = am.max_size;
+        pointer = am.pointer;
+        array_inited = am.array_inited;
+        sum = am.sum;
+        sum_square = am.sum_square;
+        double * tmp = closes;
+        closes = am.closes;
+        am.closes = tmp;
+        return *this;
+    }
+
+    constexpr double get_close() {return this->closes[this->pointer];}
+
+    constexpr double get_close(const int offset) {
+        if (pointer>=offset) return closes[pointer-offset];
+        else return closes[pointer-offset+max_size];
+    }
+
+    constexpr void update_bar(const BarData& bar) {
+        size++; pointer++;
+        if (pointer == max_size) [[unlikely]] {pointer=0;array_inited=true;}
+        if (array_inited) [[likely]] {
+            sum-=closes[pointer];
+            sum_square -= closes[pointer]*closes[pointer];
+            size--;
+        }
+        closes[pointer] = bar.close;
+        sum += bar.close;
+        sum_square += bar.close*bar.close;
+        high = std::max(bar.high, high);
+        low = std::min(bar.low, low);
+    };
+
+    constexpr double get_mean() {return sum/size;}
+
+    constexpr double get_mean(int N, int offset=0) {
+        double _sum = 0;
+        int _p = this->pointer;
+        int n = std::min(N, size-offset);
+        N = n;
+        while (offset) {_p--; offset--;}
+        while (N) {
+            if (_p<0) {_p+= this->max_size;}
+            _sum += this->closes[_p];
+            N--; _p--;
+        }
+        return _sum/n;
+    }
+
+    constexpr double get_std() {
+        return sqrt_const((sum_square - get_mean()*sum)/(size-1));
+    }
+
+    constexpr double get_std(int N, int offset=0) {
+        double _sum_sq = 0;
+        int _p = this->pointer;
+        int n = std::min(N, size-offset);
+        N = n;
+        while (offset) {_p--;offset--;}
+        while (N) {
+            if (_p<0) {_p+= this->max_size;}
+            _sum_sq += this->closes[_p]*this->closes[_p];
+            N--; _p--;
+        }
+        int mean = get_mean(n, offset);
+        return sqrt_const((_sum_sq - mean*mean*n)/(n-1));
+    }
+
+    ~SingleArrayManager_t() {
+        delete [] closes;
+    }
+};
+
+class Calculator_MACD_t: public virtual SingleArrayManager_t {
+protected:
+    struct _MACDType {double MACD,DIF,DEA;};
+    int _fast;
+    int _slow;
+    int _count;
+    double _EMA_fast;
+    double _EMA_slow;
+    double _MACD;
+    double _DEA;
+    double _DIF;
+public:
+    bool inited;
+
+    Calculator_MACD_t(int fast, int slow):
+        _fast(fast), _slow(slow), _count(1), _DEA(0), inited(false),
+        _EMA_fast(NAN), _EMA_slow(NAN), _MACD(NAN), _DIF(NAN) {}
+
+    void update() {
+        if (this->inited) [[likely]] {
+            _EMA_fast = (_EMA_fast*(_fast-1)+2*this->get_close())/(_fast+1);
+            _EMA_slow = (_EMA_slow*(_slow-1)+2*this->get_close())/(_slow+1);
+            _DIF = _EMA_fast-_EMA_slow;
+            _MACD = (_MACD*8 + _DIF*2)/10;
+            _DEA = _DIF - _MACD;
+            return;
+        }
+        if (std::isnan(_EMA_fast)) [[unlikely]] {
+            _EMA_fast = this->get_mean(_fast);
+            _EMA_slow = this->get_mean(_slow);
+            _DIF = _EMA_fast - _EMA_slow;
+        } else [[likely]] {
+            _EMA_fast = (_EMA_fast*(_fast-1)+2*this->get_close())/(_fast+1);
+            _EMA_slow = (_EMA_slow*(_slow-1)+2*this->get_close())/(_slow+1);
+            _count++;
+            if (_count < 9) {
+                _DIF += _EMA_fast-_EMA_slow;
+            } else {
+                _DIF += _EMA_fast-_EMA_slow;
+                _MACD = _DIF/9;
+                _DIF = _EMA_fast-_EMA_slow;
+                _DEA = _DIF-_MACD;
+                inited = true;
+            }
+        }
+    }
+
+    constexpr _MACDType get_MACD() {return {_MACD, _DIF, _DEA};}
+};
+
+class Calculator_RSI: public virtual SingleArrayManager_t {
+protected:
+    struct _RSIType {double RSI, RS;};
+    int _period;
+    int _count;
+    double earn;
+    double loss;
+    double RS;
+
+public:
+    bool inited;
+
+    Calculator_RSI(int period):
+        _period(period), earn(0), loss(0), inited(false), _count(0), RS(0) {
+            assert(period>=max_size && "period should strictly less than max_size");
+        }
+
+    void update() {
+        if (this->size==1) [[unlikely]] {return;}
+        _count++;
+        double diff = this->get_close() - this->get_close(1);
+        earn += std::max(0.0, diff);
+        loss -= std::min(0.0, diff);
+        if (inited) [[likely]] {
+            diff = this->get_close(_period) - this->get_close(_period+1);
+            earn -= std::max(0.0, diff);
+            loss += std::min(0.0, diff);
+            _count--;
+        }
+        RS = earn/loss;
+        if (_count==_period) {inited = true;}
+    }
+
+    constexpr _RSIType get_RSI() {return {RS, 100.0-100.0/(1+RS)};}
+
+};
+
+
+// ---------------------------------------
+// **************************************
+// ********** Union Structure ***********
+// **************************************
+
+
+template <class ..._IndexType> requires (std::is_base_of_v<SingleArrayManager_t,_IndexType> && ...)
+class IndexManager: public _IndexType... {
+
+    template <typename _Type, typename ..._Rest>
+    void _updateIndexes() {
+        if constexpr(sizeof...(_Rest)) this->_updateIndexes<_Rest...>();
+        _Type::update();
+    }
+
+public:
+
+    IndexManager(int size, _IndexType && ...instances):
+        SingleArrayManager_t(size) ,_IndexType(std::forward<_IndexType>(instances)) ... {}
+
+    template<typename ...T> requires (std::is_convertible_v<T, _IndexType> && ...)
+    IndexManager(int size, T ...val):
+        SingleArrayManager_t(size), _IndexType(val) ... {}
+
+    void update_bar(const BarData& bar) {
+        SingleArrayManager_t::update_bar(bar);
+        this->_updateIndexes<_IndexType...>();
+    }
+
+    constexpr bool is_inited() {
+        return SingleArrayManager_t::array_inited && (_IndexType::inited && ...);
     }
 };
 
 
 
+template <>
+class IndexManager<>: public SingleArrayManager_t {
+public:
+
+    IndexManager() = delete;
+
+    IndexManager(int max_length):
+        SingleArrayManager_t(max_length) {}
+
+    IndexManager(SingleArrayManager_t && am):
+        SingleArrayManager_t(std::forward<SingleArrayManager_t>(am)) {}
+
+    constexpr bool is_inited() {return this->array_inited;}
+};
+
+template <typename ...IndexTypes> requires (
+    std::is_base_of_v<SingleArrayManager_t, IndexTypes> && ...)
+class MultiSyb_IndexManager {
+    using _SybMapType=std::unordered_map<std::string, IndexManager<IndexTypes...>>;
+
+    _SybMapType _symbol_map;
+
+    //TODO construction parameters.
+
+    void new_symbol(const std::string& symbol) {
+        _symbol_map.emplace(symbol);
+    }
+
+public:
+
+    MultiSyb_IndexManager() {}
+
+
+
+};
+
+// template <typename ..._IndexTypes>
+// MultiSyb_IndexManager()
+
+// typedef SingleIndexManager<> ArrayMgr;
 
 
 typedef _Index_Calculator<SingleCalculator_MACD> Calculator_MACD;
